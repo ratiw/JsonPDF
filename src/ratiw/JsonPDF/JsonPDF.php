cedd5fd0b096995e98344a1b6609601b6a250894
Rati Wannapanop <rati.wannapanop@gmail.com>
2014-04-19 00:26:48 +0700

    refactor code

--- src/ratiw/JsonPDF/JsonPDF.php
+++ src/ratiw/JsonPDF/JsonPDF.php
@@ -5,15 +5,15 @@ class JsonPDF extends Fpdf
     protected $settings     = null;
 
     protected $header       = null;
-    
+
     protected $footer       = null;
 
     protected $tables       = null;
- 
+
     protected $varKeys      = null;
     protected $varValues    = null;
     protected $varSets      = null;
-    
+
 
     public function make($data)
     {
@@ -21,25 +21,48 @@ class JsonPDF extends Fpdf
 
         isset($data->settings) and $this->init($data->settings);
         isset($data->fonts)    and $this->addFonts($data->fonts);
-        
+
         isset($data->header) and $this->setHeader($data->header);
         isset($data->footer) and $this->setFooter($data->footer);
-        
+
         isset($data->tables) and $this->setTables($data->tables);
         isset($data->data)   and $this->setVars($data->data);
-        
+
         $this->AddPage();
         $this->renderSection($data->body);
 
         return $this;
     }
-    
+
     public function init($settings)
+    {
+        $this->initPageSettings($settings);
+
+        $this->initDisplayMode($settings);
+
+        $this->initFontSettings($settings);
+
+        // default to UTF8 encoding
+        isset($settings->utf8) || $settings->utf8 = true;
+
+        // meta-data
+        isset($settings->author)   and $this->SetAuthor($settings->author, $settings->utf8);
+        isset($settings->title)    and $this->SetTitle($settings->title, $settings->utf8);
+        isset($settings->subject)  and $this->SetSubject($settings->subject, $settings->utf8);
+        isset($settings->keywords) and $this->SetKeywords($settings->keywords, $settings->utf8);
+        isset($settings->creator)  or  $settings->creator = "JsonPDF";
+        $this->SetCreator($settings->creator, $settings->utf8);
+
+        // update settings
+        $this->settings = $settings;
+    }
+
+    protected function initPageSettings($settings)
     {
         // Number of Pages Alias
         isset($settings->{'alias-nb-pages'}) || $settings->{'alias-nb-pages'} = '{nb}';
         $this->AliasNbPages($settings->{'alias-nb-pages'});
-        
+
         // margins
         isset($settgins->{'left-margin'})  and $this->SetLeftMargin($settings->{'left-margin'});
         isset($settgins->{'top-margin'})   and $this->SetTopMargin($settings->{'top-margin'});
@@ -48,7 +71,7 @@ class JsonPDF extends Fpdf
 
         // AutoPageBreak
         if (isset($settings->{'auto-pagebreak'}))
-        {       
+        {
             if (isset($settings->{'auto-pagebreak-margin'}))
             {
                 $this->SetAutoPageBreak($settings->{'auto-pagebreak'}, $settings->{'auto-pagebreak-margin'});
@@ -58,11 +81,13 @@ class JsonPDF extends Fpdf
                 $this->SetAutoPageBreak($settings->{'auto-pagebreak'});
             }
         }
-        
+
         // page compression
         isset($settings->compression) and $this->SetCompression($settings->compression);
-        
-        // display mode
+    }
+
+    protected function initDisplayMode($settings)
+    {
         if (isset($settings->zoom))
         {
             if (isset($settings->layout))
@@ -74,7 +99,10 @@ class JsonPDF extends Fpdf
                 $this->SetDisplayMode($settings->zoom);
             }
         }
-        
+    }
+
+    protected function initFontSettings($settings)
+    {
         // default font
         if (isset($settings->{'default-font'}))
         {
@@ -82,7 +110,7 @@ class JsonPDF extends Fpdf
             $fstyle = isset($settings->{'default-font'}->style) ? $settings->{'default-font'}->style : '';
             $ffile  = isset($settings->{'default-font'}->file) ? $settings->{'default-font'}->file : false;
             $fsize  = isset($settings->{'default-font'}->size) ? $settings->{'default-font'}->size : 10;
-            
+
             if ($ffile)
             {
                 $this->AddFont($fname, $fstyle, $ffile);
@@ -91,45 +119,31 @@ class JsonPDF extends Fpdf
             {
                 $this->AddFont($fname, $fstyle);
             }
-            
+
             $this->SetFont($fname, $fstyle, $fsize);
         }
-        
-        // default to UTF8 encoding
-        isset($settings->utf8) || $settings->utf8 = true;
-        
-        // meta-data
-        isset($settings->author)   and $this->SetAuthor($settings->author, $settings->utf8);
-        isset($settings->title)    and $this->SetTitle($settings->title, $settings->utf8);
-        isset($settings->subject)  and $this->SetSubject($settings->subject, $settings->utf8);
-        isset($settings->keywords) and $this->SetKeywords($settings->keywords, $settings->utf8);
-        isset($settings->creator)  or  $settings->creator = "JsonPDF";
-        $this->SetCreator($settings->creator, $settings->utf8);
-        
-        // update settings
-        $this->settings = $settings;
     }
-    
+
     public function addFonts($fonts)
     {
         if (is_null($fonts)) return;
-        
+
         foreach ($fonts as $f)
         {
             $this->AddFont($f[0], $f[1], $f[2]);
         }
     }
-    
+
     public function setHeader($objects)
     {
         $this->header = $objects;
     }
-    
+
     public function setFooter($objects)
     {
         $this->footer = $objects;
     }
-    
+
     public function setTables($objects)
     {
         $this->tables = array();
@@ -138,12 +152,12 @@ class JsonPDF extends Fpdf
             $this->tables[$key] = $obj;
         }
     }
-    
+
     public function setVars($vars)
     {
         unset($this->varKeys);   $this->varKeys = array();
         unset($this->varValues); $this->varValues = array();
-        
+
         $i = 0;
         foreach($vars as $key => $value)
         {
@@ -159,7 +173,7 @@ class JsonPDF extends Fpdf
             }
         }
     }
-    
+
     public function renderSection($objects)
     {
         if (is_null($objects)) return;
@@ -177,16 +191,16 @@ class JsonPDF extends Fpdf
             }
         }
     }
-    
+
     public function bindVars($text)
     {
         $text = str_replace(array('{page}'), array($this->PageNo()), $text);
-        
+
         if (is_null($this->varKeys)) return $text;
-        
+
         return str_replace($this->varKeys, $this->varValues, $text);
     }
-    
+
     protected function setObjectFont($obj)
     {
         $family = isset($obj->font) ? $obj->font : $this->FontFamily;
@@ -194,19 +208,19 @@ class JsonPDF extends Fpdf
         $this->SetFont($family, $style);
         isset($obj->{'font-size'}) and $this->SetFontSize($obj->{'font-size'});
     }
-    
+
     protected function setObjectProperties($obj)
     {
         // colors
         isset($obj->{'text-color'}) and $this->callSetColorMethod('SetTextColor', $obj->{'text-color'});
 
         isset($obj->{'draw-color'}) and $this->callSetColorMethod('SetDrawColor', $obj->{'draw-color'});
-        
+
         isset($obj->{'fill-color'}) and $this->callSetColorMethod('SetFillColor', $obj->{'fill-color'});
-        
+
         // line width
         isset($obj->{'line-width'}) and $this->SetLineWidth($obj->{'line-width'});
-    
+
         // set font
         $this->setObjectFont($obj);
 
@@ -214,15 +228,15 @@ class JsonPDF extends Fpdf
         isset($obj->y) and $this->SetY($obj->y);
         isset($obj->x) and $this->SetX($obj->x);
     }
-    
+
     public function renderText($obj)
     {
         $this->setObjectProperties($obj);
-        
+
         // alignment and ln-height
         $align = isset($obj->align) ? $obj->align : 'L';
         $ln = isset($obj->ln) ? $obj->ln : 0;
-        
+
         // border
         $border = isset($obj->border) ? $obj->border : 0;
 
@@ -235,38 +249,38 @@ class JsonPDF extends Fpdf
         {
             $text = ($text == '') ? '' : iconv('UTF-8', 'ISO-8859-11', $text);
         }
-        
+
         // width & height
         $width  = isset($obj->width) ? $obj->width : 0;
         $height = isset($obj->height) ? $obj->height : 0;
-        
+
         $multiline = isset($obj->multiline) ? $obj->multiline : false;
-        
+
         if ($multiline)
         {
             $this->MultiCell(
-                $width, 
-                $height, 
-                $text, 
-                $border, 
-                $align, 
+                $width,
+                $height,
+                $text,
+                $border,
+                $align,
                 isset($obj->{'fill-color'})
             );
         }
         else
         {
             $this->Cell(
-                $width, 
-                $height, 
-                $text, 
-                $border, 
-                $ln, 
-                $align, 
+                $width,
+                $height,
+                $text,
+                $border,
+                $ln,
+                $align,
                 isset($obj->{'fill-color'})
             );
         }
     }
-    
+
     public function callSetColorMethod($method, $color)
     {
         $rgb = explode(',', $color);
@@ -274,7 +288,7 @@ class JsonPDF extends Fpdf
         {
             $rgb[$i] = intval($rgb[$i]);
         }
-        
+
         if (count($rgb) < 3)
         {
             call_user_func(array($this, $method), $rgb[0]);
@@ -284,36 +298,36 @@ class JsonPDF extends Fpdf
             call_user_func(array($this, $method), $rgb[0], $rgb[1], $rgb[2]);
         }
     }
-    
+
     public function renderLine($obj)
     {
         $this->setObjectProperties($obj);
-        
+
         isset($obj->x2) or $obj->x2 = ($this->w - $this->rMargin);
         isset($obj->y2) or $obj->y2 = $obj->y1;
-        
+
         $this->Line($obj->x1, $obj->y1, $obj->x2, $obj->y2);
     }
-    
+
     public function renderRect($obj)
     {
         $this->setObjectProperties($obj);
-        
+
         $style = isset($obj->style) ? strtoupper($obj->style) : 'D';
-        
+
         $this->Rect($obj->x, $obj->y, $obj->width, $obj->height, $style);
     }
-    
+
     public function renderImage($obj)
     {
         $x = isset($obj->x) ? $obj->x : null;
         $y = isset($obj->y) ? $obj->y : null;
         $w = isset($obj->width)  ? $obj->width : 0;
         $h = isset($obj->height) ? $obj->height : 0;
-        
+
         $this->Image($obj->url, $x, $y, $w, $h);
     }
-    
+
     private function initTable($obj)
     {
         is_null($this->tables) and $this->Error('No "tables" section.');
@@ -322,12 +336,12 @@ class JsonPDF extends Fpdf
 
         $table = $this->tables[$obj->table];
         isset($table->style) || $table->style = new stdClass;
-        
+
         $this->setObjectProperties($obj);
-        
+
         isset($table->style->{'border-color'}) and $this->callSetColorMethod('SetDrawColor', $table->style->{'border-color'});
     }
-    
+
     public function renderTable($obj)
     {
         $this->initTable($obj);
@@ -341,25 +355,25 @@ class JsonPDF extends Fpdf
         $this->initTable($obj);
         $this->_renderTableHeader($obj);
     }
-    
+
     public function renderTableBody($obj)
     {
         $this->initTable($obj);
         $this->_renderTableData($obj);
     }
-    
+
     public function setTableStyle($style)
     {
         $this->setObjectProperties($style);
     }
-    
+
     protected function _renderTableHeader($obj)
     {
         $table = $this->tables[$obj->table];
         $columns = $table->columns;
 
         $lineHeight = isset($table->style->{'title-row'}->height) ? $table->style->{'title-row'}->height : $this->lasth;
-        
+
         isset($table->style->{'title-row'}) and $this->setTableStyle($table->style->{'title-row'});
 
         $border  = isset($table->style->border) ? $table->style->border : 1;
@@ -370,41 +384,41 @@ class JsonPDF extends Fpdf
             $col = $columns[$i];
             $colWidth = isset($col->width) ? $col->width : 20;
             $this->Cell(
-                $colWidth, 
-                $lineHeight, 
+                $colWidth,
+                $lineHeight,
                 isset($col->title) ? $col->title : ucfirst($col->name),
-                $border, 
-                0, 
+                $border,
+                0,
                 isset($col->{'title-align'}) ? $col->{'title-align'} : 'C',
                 $filled
             );
         }
         $this->Ln();
     }
-    
+
     protected function _renderTableData($obj)   //$columns, $data
     {
         $table = $this->tables[$obj->table];
 
         if ( ! isset($this->varSets[$table->data])) return;
-        
+
         $data = $this->varSets[$table->data];
-        
+
         $columns = $table->columns;
-        
+
         $lineHeight = isset($table->style->{'data-row'}->height) ? $table->style->{'data-row'}->height : $this->lasth;
 
         isset($table->style->{'data-row'}) and $this->setTableStyle($table->style->{'data-row'});
-        
+
         $border  = isset($table->style->border) ? $table->style->border : 'LR';
         $filled  = isset($table->style->{'data-row'}->{'fill-color'}) ? $table->style->{'data-row'}->{'fill-color'} : false;
         $striped = isset($table->style->{'data-row'}->striped) ? $table->style->{'data-row'}->striped : false;
 
         $totalWidth = 0;
-        
+
         $maxRows = isset($table->{'max-rows'}) ? $table->{'max-rows'} : 0;
         $maxRows = ($maxRows > 0) ? $maxRows : count($data);
-        
+
         for ($r=0; $r<$maxRows; $r++)
         {
             $row = ($r < count($data)) ? $data[$r] : array();
@@ -413,11 +427,11 @@ class JsonPDF extends Fpdf
                 $col = $columns[$c];
                 $colWidth = isset($col->width) ? $col->width : 20;
                 $this->Cell(
-                    $colWidth, 
-                    $lineHeight, 
-                    ($r < count($data)) ? $row->{$col->name} : "", 
+                    $colWidth,
+                    $lineHeight,
+                    ($r < count($data)) ? $row->{$col->name} : "",
                     $border,
-                    0, 
+                    0,
                     isset($col->{'data-align'}) ? $col->{'data-align'} : 'L',
                     $filled
                 );
@@ -432,7 +446,7 @@ class JsonPDF extends Fpdf
         }
         $this->Cell($totalWidth, 0, '', 'T');
     }
-    
+
     public function render($name = '', $dest = '')
     {
         $this->Output($name, $dest);
@@ -445,21 +459,21 @@ class JsonPDF extends Fpdf
         $this->renderSection($this->header);
         isset($this->settings->{'header-height'}) and $this->SetY($this->settings->{'header-height'});
    }
-    
+
     function Footer()
     {
         $this->resetDrawing();
         isset($this->settings->{'auto-pagebreak-margin'}) and $this->SetY(-$this->settings->{'auto-pagebreak-margin'});
         $this->renderSection($this->footer);
     }
-    
+
     public function resetDrawing()
     {
         $this->SetTextColor(0, 0, 0);
         $this->SetDrawColor(0, 0, 0);
         $this->SetFillColor(255, 255, 255);
     }
- 
+
     public function snakeToCamel($str, $separator = '_')
     {
         return str_replace(' ', '', ucwords(str_replace($separator, ' ', $str)));
